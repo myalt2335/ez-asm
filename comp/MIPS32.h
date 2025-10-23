70f82250eb148fa1aba2662ee7dfd84f5c204657
Alt <118089796+myalt2335@users.noreply.github.com>
2025-10-23 16:58:55 -0400

    Enhance MIPS32 assembly definition with I/O helpers
    Updated the MIPS32 inline assembly definition to include additional convenience I/O helpers.

--- comp/MIPS32.h
+++ comp/MIPS32.h
@@ -6,7 +6,6 @@
 inline AsmDefinition MIPS32(
     "MIPS",
     "32",
-    // Registers (GPRs)
     {"$zero","$at","$v0","$v1","$a0","$a1","$a2","$a3",
      "$t0","$t1","$t2","$t3","$t4","$t5","$t6","$t7",
      "$s0","$s1","$s2","$s3","$s4","$s5","$s6","$s7",
@@ -93,8 +92,9 @@ inline AsmDefinition MIPS32(
         {"la",    "{d} = (intptr_t){s1};"},
         {"move",  "{d} = {s1};"},
 
-        // Demo I/O helpers (convention-based)
+        // I/O helpers (convention-based) (more convenience)
         {"print", "$v0 = 4; $a0 = (intptr_t){s1}; system_call();"},
         {"exit",  "$v0 = 10; system_call();"}
     }
 );
+
